9a9314b4b209dbc8d2a76d90c7b83b793d5fa475
jafar <aljuneidi.jafar@gmail.com>
2022-10-11 17:15:58 +0300

    refactoring & error handling

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        Ok(Self {
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
+}
